c6a1d3232f02b208a72bc0955cb44e41f1610748
jie12366 <2263509062@qq.com>
2019-08-19 20:13:30 +0800

    :bug: 修复对token存储的处理

--- src/service/http.js
+++ src/service/http.js
@@ -1,7 +1,27 @@
 import axios from 'axios'
 import store from '@/store/index'
 import router from '@/router'
+import { Message } from 'element-ui'
+import { RECORD_TOKEN } from '@/store/mutation-types'
 import baseURL from './base-url'
+
+function tip (msg) {
+    Message({
+        message: msg,
+        type: 'error',
+        duration: 1000
+    })
+}
+
+function toLogin () {
+    router.replace({
+        path: '/sign-in',
+        // 携带当前页面路径为查询参数，登录成功后返回当前页
+        query: {
+            redirect: router.currentRoute.fullPath
+        }
+    })
+}
 
 // 创建axios实例
 const instance = axios.create({
@@ -39,26 +59,17 @@ instance.interceptors.response.use(
         console.log(error.response.status)
             // 未授权登录
             if (error.response.status === 401) {
-                router.replace({
-                    path: '/sign-in',
-                    // 携带当前页面路径为查询参数，登录成功后返回当前页
-                    query: {
-                        redirect: router.currentRoute.fullPath
-                    }
-                }) // token过期(清除token并跳转到登录)
-            } else if (error.response.status === 403) {
-                localStorage.removeItem('token')
-                store.commit('token', null) // 把vuex中的token设置为null
+                tip('未登录')
+                toLogin()
+            } else if (error.response.status === 403) { // token过期(清除token并跳转到登录)
+                tip('登录过期，请重新登录')
+                window.localStorage.removeItem('token')
+                store.commit(RECORD_TOKEN, null) // 把vuex中的token设置为null
                 setTimeout(() => {
-                    router.replace({
-                        path: '/sign-in',
-                        // 携带当前页面路径为查询参数，登录成功后返回当前页
-                        query: {
-                            redirect: router.currentRoute.fullPath
-                        }
-                    })
+                    toLogin()
                 }, 1000) // 资源不存在
             } else {
+                tip('请求错误')
                 console.log('请求错误')
             }
         return Promise.reject(error.response)
