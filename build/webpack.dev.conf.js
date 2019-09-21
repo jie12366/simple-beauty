eaedc8ae0baa45bc4f1742dbe27b341f1d8c17b6
jie12366 <2263509062@qq.com>
2019-09-21 11:16:38 +0800

    :fire: 移除ssl相关文件

--- build/webpack.dev.conf.js
+++ build/webpack.dev.conf.js
@@ -28,7 +28,6 @@ const devWebpackConfig = merge(baseWebpackConfig, {
         { from: /.*/, to: path.posix.join(config.dev.assetsPublicPath, 'index.html') },
       ],
     },
-    https: true,
     hot: true,
     contentBase: false, // since we use CopyWebpackPlugin.
     compress: true,
